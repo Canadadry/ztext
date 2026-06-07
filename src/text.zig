48c46093d143a3a24137cf4394bd6cb63e927fdd
Canadadry <moureyjerome-spam@yahoo.fr>
2026-06-07 10:26:42 +0200

    add measure rune in test

--- src/text.zig
+++ src/text.zig
@@ -2,37 +2,45 @@ const std = @import("std");
 
 pub const AlignKind = enum { begin, middle, end };
 
-const Font = struct {
-    size: u32,
-    spacing: u32,
-    line_spacing: u32,
-    @"align": struct {
-        x: AlignKind,
-        y: AlignKind,
-    },
-};
-
-const TestCase = struct {
-    name: []const u8,
-    input: struct {
-        txt: ?[]const u8,
-        width: u32,
-        font: Font,
-    },
-    expected: struct {
-        x: u32,
-        y: u32,
-    },
-};
-
-fn measureText(txt: ?[]const u8, width: u32, font: Font) struct { x: u32, y: u32 } {
-    _ = txt;
-    _ = width;
-    _ = font;
-    return .{ .x = 0, .y = 0 };
+pub fn Font(comptime T: type, comptime default_painter: T) type {
+    return struct {
+        size: u32,
+        spacing: u32 = 0,
+        line_spacing: u32 = 0,
+        @"align": struct {
+            x: AlignKind = .begin,
+            y: AlignKind = .begin,
+        },
+        painter: T = default_painter,
+        fn measureText(font: *const @This(), txt: ?[]const u8, width: u32) struct { x: u32, y: u32 } {
+            _ = font;
+            _ = txt;
+            _ = width;
+            return .{ .x = 0, .y = 0 };
+        }
+    };
 }
 
 test "measure text" {
+    const Painter = struct {
+        pub fn measure_rune(p: *@This(), rune: u32) u32 {
+            _ = p;
+            _ = rune;
+            return 1;
+        }
+    };
+    const TestCase = struct {
+        name: []const u8,
+        input: struct {
+            txt: ?[]const u8,
+            width: u32,
+            font: Font(Painter, .{}),
+        },
+        expected: struct {
+            x: u32,
+            y: u32,
+        },
+    };
     var diagnostics = std.json.Diagnostics{};
     var reader: std.Io.Reader = .fixed(@embedFile("measure-text-testscase.json"));
     var json_reader = std.json.Reader.init(std.testing.allocator, &reader);
@@ -47,7 +55,7 @@ test "measure text" {
 
     var any_failed = false;
     for (tests.value) |tt| {
-        const got = measureText(tt.input.txt, tt.input.width, tt.input.font);
+        const got = tt.input.font.measureText(tt.input.txt, tt.input.width);
         if (got.x != tt.expected.x) {
             std.debug.print("FAIL [{s}]: x expected={d} got={d}\n", .{ tt.name, tt.expected.x, got.x });
             any_failed = true;
